ec1c06591d1b2d55e6b9ae63f96af718677eef0e
Chris Gioeli <cgioeli@gmail.com>
2018-04-16 17:49:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
   return ("I can\'t hear you!")
-  
+  else if (string === string.toUpperCase())
+  return ()
 }
 
