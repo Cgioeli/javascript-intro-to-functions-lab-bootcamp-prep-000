5463515c66d5e09d76b0b3893e85402f4e0b828a
Chris Gioeli <cgioeli@gmail.com>
2018-04-16 17:50:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function sayHiToGrandma(string) {
   return ("I can\'t hear you!")
   else if (string === string.toUpperCase())
   return ("YES INDEED!")
-  else (string === )
+  else (string === "I love you, Grandma.")
+  return ()
 }
 
