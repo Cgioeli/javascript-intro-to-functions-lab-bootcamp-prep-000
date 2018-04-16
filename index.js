6933c6135cce67f08446cba309393bfa02a44eec
Chris Gioeli <cgioeli@gmail.com>
2018-04-16 17:27:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   var uppercase = "HELLO!"
-  if uppercase.ToUppercase() === uppercase
+  if uppercase.ToUpperCase() === uppercase
   console.log("I can't hear you!")
   
   
